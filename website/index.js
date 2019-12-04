cbfef79d341d51dc49f4bc024af64f70757e2175
peyo <peteryyoon@gmail.com>
2019-12-03 16:14:29 -0800

    added all js functions and css values

---
+++ website/index.js
@@ -0,0 +1,191 @@
+// On homepage, when a user clicks on next button, display intro.
+
+function startProgram() {
+  $("#load-intro").on("click", (event) => {
+    loadIntro();
+  });
+}
+
+// On intro page, display explanations of different intelligence test types AND when user clicks on 
+// 'start quiz' button, display questions.
+
+function loadIntro() {
+  const introHtml = $(`
+    <fieldset>
+      <div class="title-content">
+        Howard Gardner, the originator of the theory of multiple intelligences, identified seven different types of intelligences. The seven can be summarized as:<br>
+          <ul>
+              <li><div class="intro-highlights">Verbal/linguistic</div>, e.g. lexical skills, formal speech, verbal debate, creative writing.</li>
+              <li><div class="intro-highlights">Body/kinesthetic (movement)</div>, e.g. body language, physical gestures, creative dance, physical exercise, drama.</li>
+              <li><div class="intro-highlights">Musical/rhythmic</div>, e.g. music performance, singing, musical composition, rhythmic patterns.</li>
+              <li><div class="intro-highlights">Logic/mathematic</div>, e.g. numerical aptitude, problem-solving, deciphering codes, abstract symbols, and formulae.</li>
+              <li><div class="intro-highlights">Visual/spatial</div>, e.g. patterns and designs, painting, drawing, active imagination, sculpture, color schemes.</li>
+              <li><div class="intro-highlights">Interpersonal (relationships with others)</div>, e.g. person-to-person communication, empathy practices, group projects, collaboration skills, receiving and giving feedback.</li>
+              <li><div class="intro-highlights">Intrapersonal (self-understanding and insight)</div>, e.g. thinking strategies, emotional processing, knowing yourself, higher-order reasoning, focusing/concentration.</li><br>
+          — Philip Carter, <i>The Complete Book of Intelligence Tests</i><br>
+          </ul>
+          Creativity and memory are not included in the above seven, but sometimes have been referred to as the eighth and ninth intelligences. The quiz will be a short exploration of verbal, numerical, logical, and creative intelligences.<br><br>Have fun!
+      </div>
+      <div class="around-the-button">
+        <button type="button" id="start-question">Start quiz ></button>
+      </div>
+    </fieldset>
+  `);
+  $("main").html(introHtml);
+  startQuiz();
+}
+
+// On intro page, when a user clicks on start button, display questions.
+
+function startQuiz() {
+  $("#start-question").on("click", (event) => {
+    displayQuestion();
+  });
+}
+
+// Display question type, explanation, and question.
+
+function displayQuestion() {
+  let question = STORE.questions[STORE.currentQuestion];
+  displayQuestionNumberAndScore();
+  const questionHtml = $(`
+    <div>
+      <form id="js-questions" class="question-form">
+        <fieldset>
+          <div class="question-type">${question.questionType}</div>
+          <div class="question-explanation">${question.questionExplanation}</div>
+          <div class="question-line">${question.question.text}</div>
+          <div class="question-image">${question.question.image}</div>
+          <div class="js-choices"></div>
+          <div class="correct-explanation"></div>
+          <div class="around-the-button">
+            <button type="submit" class="submit-button">Submit ></button>
+            <button type = "button" class="next-question-button">Next ></button>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  `);
+$("main").html(questionHtml);
+showChoices();
+$(".next-question-button").hide();
+}
+
+// Display answer choices.
+
+function showChoices()
+{
+  let question = STORE.questions[STORE.currentQuestion];
+  for(let i=0; i<question.choices.length; i++)
+  {
+    $(".js-choices").append(`
+      <input type="radio" name="choices" id="choice${i}" value="${question.choices[i].answerLetter} ${question.choices[i].answerChoice} ${question.choices[i].answerImage}">
+      <label for="choice${i}">${question.choices[i].answerLetter} ${question.choices[i].answerChoice} ${question.choices[i].answerImage}</label><br>
+    `);
+    $(".correct-explanation").append(`
+      <span id="js-explanation${i}"></span>
+    `);
+  }
+}
+
+// Display question number and number of correct answers.
+
+function displayQuestionNumberAndScore() {
+  const questionAndScoreHtml = $(`
+    <div class="top-of-page">
+      <h1>Thinkful Quiz App</h1>
+      <div class="question-number">Question ${STORE.currentQuestion + 1} of ${STORE.questions.length}</div>
+      <div class="score">Correct ${STORE.score}</div>
+      <div class="incorrect">Incorrect ${STORE.incorrect}</div>
+    </div>`);
+  $("header").html(questionAndScoreHtml);
+}
+
+// Check if the chosen choice is correct/incorrect and display if correct/incorrect and explanation.
+
+function checkSelectedChoice() {
+  $("body").on("submit","#js-questions", function(event) {
+    
+    event.preventDefault();
+    let currentQ = STORE.questions[STORE.currentQuestion];
+    let selectedChoice = $("input[name=choices]:checked").val();
+    if (!selectedChoice) {
+      alert("Choose an answer.");
+      return;
+    }
+
+    let id_num = currentQ.choices.findIndex(i => i === selectedChoice);
+    let id = "#js-explanation" + ++id_num;
+    $("span").removeClass("correct-explanation");
+    if(selectedChoice === currentQ.answer.answerLetter + " " + currentQ.answer.answerChoice + " " + currentQ.answer.answerImage) {
+      STORE.score++; 
+      $(`${id}`).append(`<b><i>Right!</b></i> :)<br><br><span class="answer-explanation-bg">${currentQ.answer.answerImage} ${currentQ.answerExplanation}</span>`);
+      $(`${id}`).addClass("correct-explanation");
+    }
+
+    else {
+      STORE.incorrect++;
+      $(`${id}`).append(`<b><i>Wrong!</b></i> :/<br><br><span class="answer-bg">The answer is <b>${currentQ.answer.answerLetter} ${currentQ.answer.answerChoice}</b></span><br><br><span class="answer-explanation-bg">${currentQ.answer.answerImage} ${currentQ.answerExplanation}</span>`);
+      $(`${id}`).addClass("correct-explanation");
+    }
+
+    STORE.currentQuestion++;
+    $(".score").text(`Correct ${STORE.score}`);
+    $(".incorrect").text(`Incorrect ${STORE.incorrect}`);
+    $(".submit-button").hide();
+    $("input[type=radio]").attr('disabled', true);
+    $(".next-question-button").show();
+  });
+}
+
+// Display link to more test examples, display that the test is complete and score.
+
+function displayResults() {
+  let resultHtml = $(`
+    <div class="results">
+      <form id="js-restart">
+        <fieldset>
+          <div class="title-content">For additional intelligence types, <i>The Complete Book of Intelligence Tests</i> can be found <a href="http://elibrary.bsu.az/books_rax/N_357.pdf">here</a>.</div>
+          <div class="final">Your result is <span id="final-score"></span>%.
+            <script>
+              let x = Math.floor(${STORE.score}/7*100);
+              document.getElementById("final-score").innerHTML = x;
+            </script>
+          </div>
+          <div class="around-the-button">
+            <button type="button" class="restart-button">Restart quiz ></button>
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  `);
+  STORE.currentQuestion = 0;
+  STORE.score = 0;
+  STORE.incorrect = 0;
+  $("main").html(resultHtml);
+}
+
+// Check if end of questions list is reached.
+
+function checkQuestionList() {
+  $("body").on("click",".next-question-button", (event) => {
+    STORE.currentQuestion === STORE.questions.length?displayResults(): displayQuestion();
+  });
+}
+
+// Restart quiz and reset question number and correct/incorrect variables.
+
+function restartQuiz() {
+  $("body").on("click",".restart-button", (event) => {
+    loadIntro();
+  });
+}
+
+function initiateApp() {
+  startProgram();
+  checkSelectedChoice();
+  checkQuestionList();
+  restartQuiz();
+}
+
+$(initiateApp);
--- website/store.js
+++ website/store.js
@@ -3,41 +3,40 @@ const STORE = {
     questions: [
         // 1    
         {
-            questionType: "Verbal aptitue - Classification",
+            questionType: "Verbal aptitude - Classification",
             questionExplanation: "Mastery of words is seen by many as having the possession of the ability to produce order out of chaos. Because of this, it is argued that the command of vocabulary is seen as a true measure of intelligence. The following exercises will test basic verbal aptitude. You are given a list of five words. Choose which of the five words is the odd one out.",
             question: 
             {
                 text: "Choose which of the five words is the odd one out.",
-                image: "https://placehold.it/300",
-                alt: "There is no image here.",
+                image: "",
             },
             choices: [
-                {questionLetter: "a", answer: "calm"},
-                {questionLetter: "b", answer: "quiet"},
-                {questionLetter: "c", answer: "relaxed"},
-                {questionLetter: "d", answer: "serene"},
-                {questionLetter: "e", answer: "unruffled"}
+                {answerLetter: "(a)", answerChoice: "calm", answerImage: ""},
+                {answerLetter: "(b)", answerChoice: "quiet", answerImage: ""},
+                {answerLetter: "(c)", answerChoice: "relaxed", answerImage: ""},
+                {answerLetter: "(d)", answerChoice: "serene", answerImage: ""},
+                {answerLetter: "(e)", answerChoice: "unruffled", answerImage: ""},
             ],
-            answer: {questionLetter: "b", answer: "quiet"},
+            answer: {answerLetter: "(b)", answerChoice: "quiet", answerImage: ""},
             answerExplanation: "'Quiet' is the odd one out, as the others mean the same thing. Although, one being quiet does not always mean that one is calm, relaxed, serene, or unruffled. One could be extremely upset and annoyed but remain quiet.",
         },
         // 2
         {
-            questionType: "Verbal aptitue - Classification",
+            questionType: "Verbal aptitude - Classification",
+            questionExplanation: "Mastery of words is seen by many as having the possession of the ability to produce order out of chaos. Because of this, it is argued that the command of vocabulary is seen as a true measure of intelligence. The following exercises will test basic verbal aptitude. You are given a list of five words. Choose which of the five words is the odd one out.",
             question:
             {
                 text: "Choose which of the five words is the odd one out.",
-                image: "https://placehold.it/300",
-                alt: "There is no image for this question.",
+                image: "",
             },
             choices: [
-                {questionLetter: "a", answer: "abode"},
-                {questionLetter: "b", answer: "dwelling"},
-                {questionLetter: "c", answer: "house"},
-                {questionLetter: "d", answer: "residence"},
-                {questionLetter: "e", answer: "street"}
+                {answerLetter: "(a)", answerChoice: "abode", answerImage: ""},
+                {answerLetter: "(b)", answerChoice: "dwelling", answerImage: ""},
+                {answerLetter: "(c)", answerChoice: "house", answerImage: ""},
+                {answerLetter: "(d)", answerChoice: "residence", answerImage: ""},
+                {answerLetter: "(e)", answerChoice: "street", answerImage: ""},
             ],
-            answer: {questionLetter: "e", answer: "street"},
+            answer: {answerLetter: "(e)", answerChoice: "street", answerImage: ""},
             answerExplanation: "'Street' is the odd one out, as the rest are places in which one can live. ‘Street’ is in a general term which may contain may houses, gardens, trees, and road surfaces. It is mostly used as a means to get from one place to another.",
         },
         // 3
@@ -47,36 +46,35 @@ const STORE = {
             question:
             {
                 text: "0, 1, 4, 9, 16, 25, 36, 49, ?",
-                image: "https://placehold.it/300",
-                alt: "There is no image for this question.",
+                image: "",
             },
             choices: [
-                {questionLetter: "a", answer: "54"},
-                {questionLetter: "b", answer: "59"},
-                {questionLetter: "c", answer: "62"},
-                {questionLetter: "d", answer: "64"},
-                {questionLetter: "e", answer: "none of the above"}
+                {answerLetter: "(a)", answerChoice: "54", answerImage: ""},
+                {answerLetter: "(b)", answerChoice: "59", answerImage: ""},
+                {answerLetter: "(c)", answerChoice: "62", answerImage: ""},
+                {answerLetter: "(d)", answerChoice: "64", answerImage: ""},
+                {answerLetter: "(e)", answerChoice: "none of the above", answerImage: ""},
             ],
-            answer: {questionLetter: "d", answer: "64"},
+            answer: {answerLetter: "(d)", answerChoice: "64", answerImage: ""},
             answerExplanation: "Add 1, 3, 5, 7, etc."
         },
         // 4
         {
             questionType: "Numberical aptitude - Number sequence",
+            questionExplanation: "Numerical tests are regarded as being culture-fair or culture-free. These questions will test one’s ability to deal with problems in a structured and analytical way. Identify a pattern that is occurring in the sequence. The numbers may be progressing, or they may be decreasing, and in some cases, they may be both progressing and decreasing within the sequence. Fill in the missing number(s) indicated by the question mark(s).",
             question:
             {
                 text: "14, 16, 28, 32, 42, 48, 56, 64, ?, ?",
-                image: "https://placehold.it/300",
-                alt: "There is no image for this question.",
+                image: "",
             },
             choices: [
-                {questionLetter: "a", answer: "74, 76"},
-                {questionLetter: "b", answer: "72, 78"},
-                {questionLetter: "c", answer: "70, 80"},
-                {questionLetter: "d", answer: "72, 82"},
-                {questionLetter: "e", answer: "none of the above"}
+                {answerLetter: "(a)", answerChoice: "74, 76", answerImage: ""},
+                {answerLetter: "(b)", answerChoice: "72, 78", answerImage: ""},
+                {answerLetter: "(c)", answerChoice: "70, 80", answerImage: ""},
+                {answerLetter: "(d)", answerChoice: "72, 82", answerImage: ""},
+                {answerLetter: "(e)", answerChoice: "none of the above", answerImage: ""},
             ],
-            answer: {questionLetter: "c", answer: "70, 80"},
+            answer: {answerLetter: "(c)", answerChoice: "70, 80", answerImage: ""},
             answerExplanation: "There are two interwoven sequences, add 14 starting at 14 and add 16 starting at 16."
         },
         // 5
@@ -86,18 +84,17 @@ const STORE = {
             question:
             {
                 text: "elk, mink, mouse, gibbon, panther. Which creature comes next? Is it:",
-                image: "https://placehold.it/300",
-                alt: "There is no image for this question.",
+                image: "",
             },
             choices: [
-                {questionLetter: "a", answer: "squirrel"},
-                {questionLetter: "b", answer: "tortoise"},
-                {questionLetter: "c", answer: "tigress"},
-                {questionLetter: "d", answer: "wildebeest"},
-                {questionLetter: "e", answer: "platypus"},
-                {questionLetter: "f", answer: "aardvark"},
+                {answerLetter: "(a)", answerChoice: "squirrel", answerImage: ""},
+                {answerLetter: "(b)", answerChoice: "tortoise", answerImage: ""},
+                {answerLetter: "(c)", answerChoice: "tigress", answerImage: ""},
+                {answerLetter: "(d)", answerChoice: "wildebeest", answerImage: ""},
+                {answerLetter: "(e)", answerChoice: "platypus", answerImage: ""},
+                {answerLetter: "(f)", answerChoice: "aardvark", answerImage: ""},
             ],
-            answer: {questionLetter: "b", answer: "tortoise"},
+            answer: {answerLetter: "(b)", answerChoice: "tortoise", answerImage: ""},
             answerExplanation: "The name of each creature starts with the letter that comes two letters in the alphabet after the last letter of the previous creature. Also, the name of each creature has one more letter than the previous creature."
         },
         // 6
@@ -106,19 +103,18 @@ const STORE = {
             questionExplanation: "This section will test one’s ability to recognize patterns and designs, one’s ability to think logically but at the same time to explore with an open mind the various possibilities that might lead to a correct answer. A matrix is an array of squares in which one of the squares has been omitted, and where you must choose the correct missing square from a number of choices. Study the matrix to decide what pattern is occurring, either by looking across each line and down each column, looking at the array as a while or looking at the relationship between different squares within the array.",
             question:
                 {
-                    text: "Which is the missing section?",
-                    image: "images/questions-answers/q-06/question06.png",
-                    alt: "A pattern and design. This question may be difficult for the visually impared."
+                    text: "Which is the missing section?<br>",
+                    image: `<img src='images/questions-answers/q-06/question06.png' alt='A pattern and design. This question may be difficult for the visually impared.'>`,
                 },
             choices: [
-                {questionLetter: "a", answerImage: "images/questions-answers/q-06/answers/01answerA.png", alt: "A piece that may fit into the pattern shown in the question. This question may be difficult for the visually impared."},
-                {questionLetter: "b", answerImage: "images/questions-answers/q-06/answers/02answerB.png", alt: "A piece that may fit into the pattern shown in the question. This question may be difficult for the visually impared."},
-                {questionLetter: "c", answerImage: "images/questions-answers/q-06/answers/03answerC.png", alt: "A piece that may fit into the pattern shown in the question. This question may be difficult for the visually impared."},
-                {questionLetter: "d", answerImage: "images/questions-answers/q-06/answers/03answerD.png", alt: "A piece that may fit into the pattern shown in the question. This question may be difficult for the visually impared."},
-                {questionLetter: "e", answerImage: "images/questions-answers/q-06/answers/03answerE.png", alt: "A piece that may fit into the pattern shown in the question. This question may be difficult for the visually impared."},
-                {questionLetter: "f", answerImage: "images/questions-answers/q-06/answers/03answerF.png", alt: "A piece that may fit into the pattern shown in the question. This question may be difficult for the visually impared."},
+                {answerLetter: "(a)", answerChoice: "", answerImage: "<img src='images/questions-answers/q-06/answers/01answerA.png' alt='A pattern and design. This question may be difficult for the visually impared.'><br>"},
+                {answerLetter: "(b)", answerChoice: "", answerImage: "<img src='images/questions-answers/q-06/answers/02answerB.png' alt='A pattern and design. This question may be difficult for the visually impared.'><br>"},
+                {answerLetter: "(c)", answerChoice: "", answerImage: "<img src='images/questions-answers/q-06/answers/03answerC.png' alt='A pattern and design. This question may be difficult for the visually impared.'><br>"},
+                {answerLetter: "(d)", answerChoice: "", answerImage: "<img src='images/questions-answers/q-06/answers/04answerD.png' alt='A pattern and design. This question may be difficult for the visually impared.'><br>"},
+                {answerLetter: "(e)", answerChoice: "", answerImage: "<img src='images/questions-answers/q-06/answers/05answerE.png' alt='A pattern and design. This question may be difficult for the visually impared.'><br>"},
+                {answerLetter: "(f)", answerChoice: "", answerImage: "<img src='images/questions-answers/q-06/answers/06answerF.png' alt='A pattern and design. This question may be difficult for the visually impared.'>"},
             ],
-            answer: {questionLetter: "b", answerImage: "images/questions-answers/q-06/answers/02answerB.png", alt: "A piece that may fit into the pattern shown in the question. This question may be difficult for the visually impared."},
+            answer: {answerLetter: "(b)", answerChoice: "", answerImage: "<img src='images/questions-answers/q-06/answers/02answerB.png' alt='A pattern and design. This question may be difficult for the visually impared.'><br>"},
             answerExplanation: "Looking across and down, alternative circle combinations are repeated with the addition of an extra circle."
         },
         // 7
@@ -128,18 +124,19 @@ const STORE = {
             question:    
                 {
                     text: "How many circles appear above?",
-                    image: "images/questions-answers/q-07/question07.png",
-                    alt: "A pattern and design. This question may be difficult for the visually impared."
+                    image: `<img src='images/questions-answers/q-07/question07.png' alt='A pattern and design. This question may be difficult for the visually impared.'>`
                 },
             choices: [
-                {questionLetter: "a", answer: "11"},
-                {questionLetter: "b", answer: "1"},
-                {questionLetter: "c", answer: "0"},
+                {answerLetter: "(a)", answerChoice: "11", answerImage: ""},
+                {answerLetter: "(b)", answerChoice: "1", answerImage: ""},
+                {answerLetter: "(c)", answerChoice: "0", answerImage: ""},
             ],
-            answer: {questionLetter: "b", answer: "1"},
+            answer: {answerLetter: "(b)", answerChoice: "1", answerImage: ""},
             answerExplanation: "There is only one complete circle, in the middle of the image. The rest are arches and incomplete circles."
         },
     ],
     currentQuestion: 0,
-    score: 0
-  };
+    score: 0,
+    incorrect: 0
+};
+
